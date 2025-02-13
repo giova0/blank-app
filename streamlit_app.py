65828f405954089c632678f676e37fbc3316407c
giova0 <giova0@gmail.com>
2025-02-13 23:42:28 +0000

    titulo en español y seleccionador de cantidades

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Bases de datos Marruecos IED")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "## Esto está en Mark down. Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+cantidad = st.slider('Selecciona la cantidad')
+
+st.write(f'la cantidad seleccionada es: {cantidad}')
+
+for i in range(cantidad):
+    st.button(f'{i}')
